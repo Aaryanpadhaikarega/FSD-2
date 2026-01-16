2f545c4878888227e167ed7f2fa2336bf5ebf0d8
Aaryanpadhaikarega <aaryanranaut4@gmail.com>
2026-01-16 09:43:04 +0530

    path fix for build

--- experiment_0/src/App.jsx
+++ experiment_0/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import SinglePageApp from './assets/components/spa.jsx'
+import SinglePageApp from './assets/components/Spa.jsx'
 import Home from './assets/components/Home.jsx'
 function App() {
   const [count, setCount] = useState(0)
